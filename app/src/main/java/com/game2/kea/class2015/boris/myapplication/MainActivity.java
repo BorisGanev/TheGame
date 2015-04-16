15bc30280ad152da038d6fb745e324a7b8a63fa0
DingoTheMagicDragon <boris.ganev@hotmail.com>
2015-04-16 16:24:19 +0200

    my dumb things which probably dont work

--- app/src/main/java/com/game2/kea/class2015/boris/myapplication/MainActivity.java
+++ app/src/main/java/com/game2/kea/class2015/boris/myapplication/MainActivity.java
@@ -16,9 +16,9 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.asd);
-
-        mediaPlayer.start();
+//        MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.asd);
+//
+//        mediaPlayer.start();
     }
 
 
